1681285c99b2f21c750fc02cce7860db512e2e21
Pragnesh Rathod <39561106+PragneshRathod901@users.noreply.github.com>
2024-04-06 01:21:53 +0530

    Theme setup

---
+++ src/Theme.js
@@ -0,0 +1,30 @@
+import { createTheme } from "@mui/material";
+
+const Theme = createTheme({
+  palette: {
+    primary: {
+      main: "#2AA7FF",
+      dark: "#14BEF0",
+      light: "#8CCFFF",
+      contrastText: "#FFFFFF",
+    },
+    secondary: {
+      main: "#1B3C74",
+      light: "#77829D",
+      dark: "#102851",
+    },
+    success: {
+      main: "#01A400",
+    },
+    mode: "light",
+    text: {
+      primary: "#1B3C74",
+      secondary: "#2AA7FF",
+      disabled: "#77829D",
+    },
+  },
+  typography: {
+    fontFamily: ["Poppins", "sans-serif"],
+  },
+});
+export default Theme;
